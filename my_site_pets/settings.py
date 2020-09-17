6efa75843bce05a2449e570ae0abd5641bfe61ab
Natalya <mot_a@bk.ru>
2020-09-17 21:26:10 +0300

    commit

--- my_site_pets/settings.py
+++ my_site_pets/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'p--od(_6f0=9(v+xz4z2hlpdfw!9)loxw893&b0(vjn3w%m_aj'
-
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
